393c1fb5b9d77ba6d81e630303886a9b3246308e
kokasmark <mark.kokas04@gmail.com>
2023-09-15 13:20:04 +0200

    Kód nézet és Kommentelési lehetőség

--- src/Block.js
+++ src/Block.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 import html_bg from './assets/html_bg.png'
 import css_bg from './assets/css_bg.png'
 import js_bg from './assets/js_bg.png'
+import code_bg from './assets/code_bg.png'
 import html_logo from './assets/html_logo.png'
 import css_logo from './assets/css_logo.png'
 import js_logo from './assets/js_logo.png'
 import code_icon from './assets/code_icon.png'
+import note_bg from './assets/note.png'
 import App from './App';
 import Draggable from 'react-draggable';
 
@@ -18,15 +20,25 @@ function cycleTag(b){
    return 0;
   }
 }
+function isCommentCheck(b){
+  if(b.state.tag == '//' && b.state.view != 4){
+    b.setState({
+      tag: b.state.tag,
+      view: 4,
+      isComment: true
+    })
+  }
+}
 export default class Block extends Component{
 
   state = {
     tag: '',
     view: 0,
-    startPos: {x: 100, y: 100}
+    startPos: {x: 100, y: 100},
+    isComment: false
   }
   static getDerivedStateFromProps(props,state) {
-    return {tag: props.tag, startPos: props.startPos};
+    return {tag: props.tag};
   }
   cycle = () => {
       if(this.state.view < 2)
@@ -42,24 +54,24 @@ export default class Block extends Component{
         })
       }
   }
-
   render(){
       return (
-      <Draggable>
-      <div className="Block" currentView={this.state.view}> 
-      <img alt='' draggable = 'false' src={this.state.view == 0 && html_logo || this.state.view == 1 && css_logo || this.state.view == 2 && js_logo} style={{width: 40, height: 40, position: 'relative', top: 45, right: -118}} onClick={this.cycle}/>
-      <img alt='' draggable = 'false' src={code_icon} style={{width: 40, height: 40, position: 'relative', top: 45, right: 118}} onClick={() =>  this.setState({tag: this.state.tag,view: 4})}/>
+        <div onLoad={() => isCommentCheck(this)}>
+      {this.state.view != 4 && <Draggable> 
+      <div className="Block" currentView={this.state.view}>
+      <img alt='' draggable = 'false' src={code_icon} style={{width: 40, height: 40, position: 'relative', top: 45, right: 78}} onClick={() =>  {{this.state.view != 3 && this.setState({tag: this.state.tag,view: 3})} {this.state.view == 3 && this.setState({tag: this.state.tag,view: 0})}}}/>
+      { this.state.view < 3 && <img alt='' draggable = 'false' src={this.state.view == 0 && html_logo || this.state.view == 1 && css_logo || this.state.view == 2 && js_logo } style={{width: 40, height: 40, position: 'relative', top: 45, right: -78}} onClick={this.cycle}/>}
         {this.state.view == 0 &&
           <div>
             <img src={html_bg} draggable="false" className='block_bg'/>
-            <p style={{position: 'relative', top: -85, fontWeight: 'bold',color: 'white'}}>&lt;{this.state.tag}&gt; <input style={{width: 100, borderStyle: 'none', outline: 'none',borderRadius: 10}} type='text'/> &lt;/{this.state.tag}&gt;</p>
+            <p style={{position: 'relative', top: -85, fontWeight: 'bold',color: 'white'}}>&lt;{this.state.tag}&gt; <input spellcheck="false" style={{width: 100, borderStyle: 'none', outline: 'none',borderRadius: 10}} type='text'/> &lt;/{this.state.tag}&gt;</p>
           </div>
          }
 
         {this.state.view == 1 &&
           <div>
             <img src={css_bg} draggable="false"/>
-            <textarea style={{position: 'relative', top: -95,width: 200, height: 75,borderStyle: 'none', outline: 'none',borderRadius: 10, backgroundColor: "#3b6feb", color: "white"}} />
+            <textarea spellcheck="false" style={{position: 'relative', top: -95,width: 200, height: 75,borderStyle: 'none', outline: 'none',borderRadius: 10, backgroundColor: "#3b6feb", color: "white"}} />
           </div>
          }
          {this.state.view == 2 &&
@@ -67,8 +79,24 @@ export default class Block extends Component{
             <img src={js_bg} draggable="false"/>
           </div>
          }
+         {this.state.view == 3 &&
+          <div>
+            <img src={code_bg} draggable="false"/>
+            <textarea spellcheck="false" style={{position: 'relative', top: -95,width: 200, height: 75,borderStyle: 'none', outline: 'none',borderRadius: 10, backgroundColor: "#313134", color: "white"}} />
+          </div>
+         }
       </div>
       </Draggable>
+      }
+      {this.state.view == 4 &&     
+        <Draggable>
+          <div>
+            <img style={{transform: 'scale(.6)'}} alt='' draggable = 'false' src={note_bg}/>
+            <textarea style={{transform: 'scale(.6)',position: 'relative', top: -80, right: 278,width: 200, height: 150  ,borderStyle: 'none', outline: 'none',borderRadius: 10, backgroundColor: "white", color: "black", resize: 'none',fontSize: 20}} spellcheck="false" placeholder='This is a Comment!'/>
+          </div>
+          </Draggable>  
+         }
+      </div>
     );
       }
   }
